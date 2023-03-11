aff94bef3f4c2ae6659ba51b6ce04044e1661e30
Peerawat Wongmek <78299267+peerawat2645@users.noreply.github.com>
2023-03-11 22:53:43 +0700

    updatePage

--- ITM/app.py
+++ ITM/app.py
@@ -1,27 +1,30 @@
 import numpy as np
-from flask import Flask, request, render_template, Response , jsonify, make_response , session, redirect, flash
+from flask import Flask, request, render_template, Response , jsonify, Blueprint , session, redirect, flash
 import jwt
+import re
 from flask_mysqldb import MySQL
 from datetime import datetime, timedelta
 from functools import wraps
+from datetime import datetime, date
 import pickle
 import cv2
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'x123'
-app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(seconds=20)
-app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'test'
+app.config['MYSQL_DB'] = 'test' #name DB
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 mysql = MySQL(app)
 
 @app.route('/')
 def index():
+    error = None
     session.clear();
     if not session.get('logged_in'):
         return render_template('login.html')
@@ -41,41 +44,17 @@ def token_required(func):
             return jsonify({'Alert!' : 'Invalid Token!'}),403
     return decorated
 
-# User Database Route
-# this route sends back list of users
-@app.route('/user', methods =['GET'])
-@token_required
-def get_all_users(current_user):
-    # querying the database
-    # for all the entries in it
-    cur = mysql.connection.cursor()
-    cur.execute("select u.userIdu.name,u.email from users u")
-    fetchdata = cur.fetchall()
-    cur.close()
-    # converting the query objects
-    # to list of jsons
-    output = []
-    for user in users:
-        # appending the user data json
-        # to the response list
-        output.append({
-            'public_id': user.public_id,
-            'name' : user.name,
-            'email' : user.email
-        })
-  
-    return jsonify({'users': output})
-
 @app.route('/login', methods=['POST'])
 def login():
     error = None
     cur = mysql.connection.cursor()
-    cur.execute("select u.username,u.password,u.email from users u")
+    cur.execute("select u.username,u.password,u.email,u.userID from users u")
     fetchdata = cur.fetchall()
     cur.close()
     for data in fetchdata:      
         if request.form['username'] == data[0] and request.form['password'] == data[1]:
             session['logged_in'] = True
+            session['userID'] = data[3]
             token = jwt.encode({
                 'user':data[0],
                 'email':data[2],
@@ -86,8 +65,8 @@ def login():
             return redirect("/index", code=302)
         else: 
             #return jsonify({'token' : 'Invalid credentials'})
-            error = 'Invalid credentials'
-            return render_template('login.html', error=error)
+            flash(u'Invalid password provided', 'error')
+            return render_template('login.html')
         
 
 @app.route('/registerForm')
@@ -104,32 +83,50 @@ def checkUsername(username):
     except Exception:
         return False
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST','GET'])
 def register():
-    username = request.form['username']
-    if(checkUsername(username)):
-        return 'username exist'
-    password = request.form['password']
-    email = request.form['email']
-    confirmPassword = request.form['confirmPassword']
-    if (password != confirmPassword):
-        return 'not match password'
-    name = request.form['name']
-    surname = request.form['surname']
-    age = request.form['age']
-    values = (email,username,password,name,surname,age)
-    cur = mysql.connection.cursor()
-
-    cur.execute("INSERT INTO users(email,username,password,name,surname,age) VALUES(%s,%s,%s,%s,%s,%s)", values)
-    mysql.connection.commit()
-    cur.close()
-    return redirect("/", code=302)
+    mesage = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'name' in request.form and 'surname' in request.form and 'age' in request.form and 'height' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        name = request.form['name']
+        surname = request.form['surname']
+        age = request.form['age']
+        height = request.form['height']
+        confirmPassword = request.form['password_confirm']
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM users WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if not age.isnumeric():
+            mesage = 'age is not number'
+        elif not height.isnumeric():
+            mesage = 'height is not number'
+        elif account:
+            mesage = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            mesage = 'Invalid email address !'
+        elif len(password) < 8:
+            mesage = 'password length less than 8 character'
+        elif len(confirmPassword) < 8:
+            mesage = 'confirmpassword length less than 8 character'
+        elif password != confirmPassword:
+            mesage = 'password not match'
+        else:
+            values = (email,username,password,name,surname,age,height)
+            cursor.execute("INSERT INTO users(email,username,password,name,surname,age,height) VALUES(%s,%s,%s,%s,%s,%s,%s)", values)
+            mysql.connection.commit()
+            return redirect('/')
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    return render_template('register.html', mesage = mesage)
 
 
 @app.route('/logout')
 def logout():
     if 'logged_in' in session:
-        session.pop('logged_in',False)
+        session.pop('logged_in',None)
+        session.pop('userID',None)
     return redirect("/", code=302)
 
 #Authen
@@ -150,23 +147,97 @@ def home():
 @app.route("/profile")
 def profile():
     cur = mysql.connection.cursor()
-    cur.execute("select u.name,u.surname from users u")
+    cur.execute("select u.name,u.surname,u.age,u.height from users u where u.userID ="+str(session['userID']))
+    fetchdata = cur.fetchall()
+    cur.execute("SELECT huc.score,huc.date from usercourse uc INNER join historyusercourse huc on huc.userCourseID=uc.userCourseID WHERE uc.userID=%s AND huc.date >= (SELECT ADDDATE(Current_date, INTERVAL -8 DAY)) AND huc.date <  (SELECT ADDDATE(Current_date, INTERVAL 1 DAY))order by huc.date;",str(session['userID']))
+    data = cur.fetchall()
+    cur.close()
+    labels  = [row[1].strftime('%m/%d/%Y') for row in data]
+    data = [row[0] for row in data]
+    
+    return render_template('profile.html',user=fetchdata , labels = labels , data = data)
+
+@app.route("/editProfile")
+def editProfile():
+    cur = mysql.connection.cursor()
+    cur.execute("select u.name,u.surname,u.age,u.height from users u where u.userID ="+str(session['userID']))
     fetchdata = cur.fetchall()
     cur.close()
-    #return render_template('profile.html')
-    return jsonify(session)
+    return render_template('editProfile.html',user=fetchdata)
 
-@app.route("/select")
+@app.route("/save",methods=['POST','GET'])
+def saveProfile():
+    mesage = ''
+    if 'name' in request.form and 'surname' in request.form and 'age' in request.form and 'height' in request.form:
+        name = request.form['name']
+        surname = request.form['surname']
+        age = request.form['age']
+        height = request.form['height']
+        cur = mysql.connection.cursor()
+        if not age.isnumeric():
+            mesage = 'age is not number'
+        elif not height.isnumeric():
+            mesage = 'height is not number'
+        else:
+            values = (name,surname,int(age),int(height),int(session['userID']))
+            cur.execute("Update users set name=%s,surname =%s,age=%s,height=%s where users.userID = %s",values)
+            mysql.connection.commit()
+            return redirect('/profile')
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    return redirect('/editProfile')
+
+@app.route("/select",methods=['POST','GET'])
 def select():
-    return render_template('select.html')
+    courseId = 1
+    cur = mysql.connection.cursor()
+    cur.execute("select distinct c.name from course c inner join posture p on p.postureID=c.posture1ID or p.postureID=c.posture2ID")
+    fetchdata = cur.fetchall()
+    cur.execute("select distinct c.name,c.posture1ID,c.posture2ID from course c inner join posture p on p.postureID=c.posture1ID or p.postureID=c.posture2ID where c.courseID="+str(courseId))
+    courseSelected = cur.fetchall()
+    cur.execute("select p.name from posture p where p.postureID="+str(courseSelected[0][1]))
+    course1 = cur.fetchall()
+    cur.execute("select p.name from posture p where p.postureID="+str(courseSelected[0][2]))
+    course2 = cur.fetchall()
+    cur.close()
+    courseNameSelected = [course1[0],course2[0]]
+    return render_template('select.html',course = fetchdata,courseSelected = courseSelected,courseNameSelected = courseNameSelected)
 
-@app.route("/preview")
-def preview():
-    return render_template('preview.html')
+@app.route("/select/<courseName>",methods=['POST','GET'])
+def seletedCourse(courseName):
+    cur = mysql.connection.cursor()
+    cur.execute("select distinct c.name from course c inner join posture p on p.postureID=c.posture1ID or p.postureID=c.posture2ID")
+    fetchdata = cur.fetchall()
+    cur.execute("select distinct c.name,c.posture1ID,c.posture2ID from course c inner join posture p on p.postureID=c.posture1ID or p.postureID=c.posture2ID where c.name='"+str(courseName)+"'")
+    courseSelected = cur.fetchall()
+    cur.execute("select p.name from posture p where p.postureID="+str(courseSelected[0][1]))
+    course1 = cur.fetchall()
+    cur.execute("select p.name from posture p where p.postureID="+str(courseSelected[0][2]))
+    course2 = cur.fetchall()
+    cur.close()
+    courseNameSelected = [course1[0],course2[0]]
+    return render_template('selectedCourse.html',courseSelected = courseSelected,courseNameSelected = courseNameSelected)
 
-@app.route("/play",methods=['POST','GET'])
-def play():
-    return render_template('play.html')
+@app.route("/preview/<courseName>",methods=['POST','GET'])
+def preview(courseName):
+    cur = mysql.connection.cursor()
+    cur.execute("select distinct c.name,c.posture1ID,c.posture2ID from course c inner join posture p on p.postureID=c.posture1ID or p.postureID=c.posture2ID where c.name='"+str(courseName)+"'")
+    courseSelected = cur.fetchall()
+    cur.execute("select p.name from posture p where p.postureID="+str(courseSelected[0][1]))
+    course1 = cur.fetchall()
+    cur.execute("select p.name from posture p where p.postureID="+str(courseSelected[0][2]))
+    course2 = cur.fetchall()
+    cur.close()
+    courseNameSelected = [course1[0],course2[0]]
+    return render_template('preview.html',courseSelected = courseSelected,courseNameSelected = courseNameSelected)
+
+@app.route("/playtask1/<courseName>",methods=['POST','GET'])
+def playtask1(courseName):
+    return render_template('play.html',courseName=courseName)
+
+@app.route("/playtask2/<courseName>",methods=['POST','GET'])
+def playtask2(courseName):
+    return render_template('play2.html')
 
 def gen():
     
